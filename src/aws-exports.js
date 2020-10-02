d629ce345a4cf6018aa02e80a9191361a3e0f411
K-itKat20 <LaurelKatie@tutanota.com>
2020-10-02 16:17:42 +0100

    Added API

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,14 @@ const awsmobile = {
     "aws_cognito_region": "eu-west-2",
     "aws_user_pools_id": "eu-west-2_MrXfpvcRx",
     "aws_user_pools_web_client_id": "65cu49dsknn1sng1784ih0jqc1",
-    "oauth": {}
+    "oauth": {},
+    "aws_cloud_logic_custom": [
+        {
+            "name": "DataVisAPI",
+            "endpoint": "https://iokinm6v9c.execute-api.eu-west-2.amazonaws.com/dev",
+            "region": "eu-west-2"
+        }
+    ]
 };
 
 
