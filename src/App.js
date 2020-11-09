5fd5a8684ebf8e1f1c97329f299eee50097719ef
A-S620 <aatikaseedat00@gmail.com>
2020-11-09 09:24:36 +0000

    Unit tests, code formatting

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 //UI Components Imports
 import UserNavigation from './components/Navigation/UserNavigation';
 import './App.css';
+import FrontNavigation from './components/Navigation/FrontNavigation';
 
 export default function App() {
     return (
         <div>
-            <UserNavigation />
+            {/*<UserNavigation />*/}
             {/*<Appbar />*/}
             {/*<Construction />*/}
+            <FrontNavigation />
         </div>
     );
 }
