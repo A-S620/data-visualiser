0955b626e5d65b5b762cfce1f11a52498a616c26
A-S620 <aatikaseedat00@gmail.com>
2020-11-14 16:04:21 +0000

    Working on notifications

--- src/domain/ImportData.tsx
+++ src/domain/ImportData.tsx
@@ -1,10 +1,22 @@
 //A class that handles the imported data.
 //Interfaces
+import { IFile } from './interfaces/IFile';
+import { IImportedFiles } from './interfaces/IImportedFiles';
 
 //Other domain components
+import { Notifications } from './Notifications';
 export class ImportData {
     private importedData: any;
     constructor(importedData: any) {
         this.importedData = importedData;
+    }
+    public validate(): Notifications {
+        const notifications: Notifications = new Notifications();
+        for (var i = 0; i < this.importedData.length; i++) {
+            if (this.importedData[i].length === 0) {
+                notifications.addNotification(`File ${this.importedData[i].name} is empty`);
+            }
+        }
+        return notifications;
     }
 }
