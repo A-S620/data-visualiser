c9fddb6f1499f65d146e744536cd873360166f1a
A-S620 <aatikaseedat00@gmail.com>
2020-11-18 16:15:44 +0000

    Working on file imports

--- src/domain/ImportData.tsx
+++ src/domain/ImportData.tsx
@@ -1,18 +1,17 @@
 //A class that handles the imported data.
 //Interfaces
 import { IFile } from './interfaces/IFile';
-import { IImportedFiles } from './interfaces/IImportedFiles';
 
 //Other domain components
 import { Notifications } from './Notifications';
 export class ImportData {
     private importedData: any;
-    constructor(importedData: any) {
+    constructor(importedData: string) {
         this.importedData = importedData;
     }
     public validate(): Notifications {
         const notifications: Notifications = new Notifications();
-        for (var i = 0; i < this.importedData.length; i++) {
+        for (var i = 0; i < this.importedData.length; i += 1) {
             if (this.importedData[i].length === 0) {
                 notifications.addNotification(`File ${this.importedData[i].name} is empty`);
             }
--- test/components/LoggedIn/Home/Home.test.js
+++ test/components/LoggedIn/Home/Home.test.js
@@ -8,26 +8,4 @@ import Home from '../../../../src/components/LoggedIn/Home/Home';
 
 describe('Home page', () => {
     const wrapper = mount(<Home />);
-    it('should have a drag and drop feature', () => {
-        //given I have a home page
-        //when I go on it
-        //it should have a drag and drop feature for documents
-
-        const dragAndDrop = wrapper.find('input');
-
-        expect(wrapper.find('input')).toBeTruthy();
-    });
-    describe('Dropzone area', () => {
-        it('should accept JSON files', () => {
-            //given I have a dropzone area
-            //when I drag a JSON file into it
-            //it should accept the document
-
-            const dropZone = wrapper.find('input');
-
-            expect(dropZone.props().type).toBe('file');
-
-            expect(dropZone.props().accept).toBe('application/json');
-        });
-    });
 });
