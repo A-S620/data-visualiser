450210870007ca53be14811dd6d69fb96ce4e842
A-S620 <aatikaseedat00@gmail.com>
2020-12-04 14:04:07 +0000

    ResetImportedDataState class created + unit tests set up

---
+++ test/domain/ReduxStateHandlers/ImportedDataHandlers/ResetImportedDataState.test.tsx
@@ -0,0 +1,9 @@
+//Imports from libraries
+import 'jsdom-global/register';
+import React from 'react';
+
+//Domain Components
+import CreateImportedDataState from '../../../../src/domain/ReduxStateHandlers/ImportedDataHandlers/CreateImportedDataState';
+import GetImportedDataState from '../../../../src/domain/ReduxStateHandlers/ImportedDataHandlers/GetImportedDataState';
+
+describe('ResetImportedDataState domain component', () => {});
