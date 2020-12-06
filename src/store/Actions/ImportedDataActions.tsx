b191d4c037e90e6356020216d871f98357ac6d09
A-S620 <aatikaseedat00@gmail.com>
2020-12-06 18:23:50 +0000

    Refactoring ImportedDataActions.tsx

--- src/store/Actions/ImportedDataActions.tsx
+++ src/store/Actions/ImportedDataActions.tsx
@@ -2,26 +2,18 @@
 //Action types
 import * as importedDataActions from './ImportedDataActionTypes';
 
-export function addDataFields(dataFields: Array<string>) {
-    return {
-        type: importedDataActions.DATA_FIELDS_ADDED,
-        payload: dataFields,
-    };
-}
-export function addDataAsArrays(dataAsArrays: Array<Array<any>>) {
-    return {
-        type: importedDataActions.DATA_AS_ARRAYS_ADDED,
-        payload: dataAsArrays,
-    };
-}
-export function addDataAsObjects(dataAsObjects: Array<object>) {
-    return {
-        type: importedDataActions.DATA_AS_OBJECTS_ADDED,
-        payload: dataAsObjects,
-    };
-}
-export function resetImportedData() {
-    return {
-        type: importedDataActions.IMPORTED_DATA_STATE_RESET,
-    };
-}
+export const addDataFields = (dataFields: Array<string>) => ({
+    type: importedDataActions.DATA_FIELDS_ADDED,
+    payload: dataFields,
+});
+export const addDataAsArrays = (dataAsArrays: Array<Array<any>>) => ({
+    type: importedDataActions.DATA_AS_ARRAYS_ADDED,
+    payload: dataAsArrays,
+});
+export const addDataAsObjects = (dataAsObjects: Array<object>) => ({
+    type: importedDataActions.DATA_AS_OBJECTS_ADDED,
+    payload: dataAsObjects,
+});
+export const resetImportedData = () => ({
+    type: importedDataActions.IMPORTED_DATA_STATE_RESET,
+});
