ea745de2f92f625fac1dc39242d8c618f4698fa7
A-S620 <aatikaseedat00@gmail.com>
2020-12-23 15:05:03 +0000

    Made Linting changes to other files in src

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-//Imports from libraries
 import React from 'react';
 
-//UI Components Imports
 import UserNavigation from './components/LoggedIn/Navigation/UserNavigation';
 import './App.css';
-import FrontNavigation from './components/NotLoggedIn/Navigation/FrontNavigation';
 
 export default function App() {
     return (
         <div>
             <UserNavigation />
-            {/*<Appbar />*/}
-            {/*<Construction />*/}
-            {/*<FrontNavigation />*/}
         </div>
     );
 }
