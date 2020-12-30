8d45bbffd8ed3a261cc4717e8d8d9e9742b54401
A-S620 <48350000+a-s620@users.noreply.github.com>
2020-12-30 14:09:52 +0000

    ILinePlottingOptions.tsx interface

---
+++ src/domain/interfaces/plotting/ILinePlottingOptions.tsx
@@ -0,0 +1,35 @@
+export interface ILinePlottingOptions {
+    xValues: string;
+    yValues: string;
+    height: number;
+    width: number;
+    colour: string;
+    opacity: number;
+    curveType: CurveType;
+    lineStyle: LineStyle;
+    lineWidth: number;
+}
+export enum CurveType {
+    curveBasis = 'curveBasis',
+    curveBasisClosed = 'curveBasisClosed',
+    curveBasisOpen = 'curveBasisOpen',
+    curveBundle = 'curveBundle',
+    curveCardinal = 'curveCardinal',
+    curveCardinalClosed = 'curveCardinalClosed',
+    curveCardinalOpen = 'curveCardinalOpen',
+    curveCatmullRom = 'curveCatmullRom',
+    curveCatmullRomClosed = 'curveCatmullRomClosed',
+    curveCatmullRomOpen = 'curveCatmullRomOpen',
+    curveLinear = 'curveLinear',
+    curveLinearClosed = 'curveLinearClosed',
+    curveMonotoneX = 'curveMonotoneX',
+    curveMonotoneY = 'curveMonotoneY',
+    curveNatural = 'curveNatural',
+    curveStep = 'curveStep',
+    curveStepAfter = 'curveStepAfter',
+    curveStepBefore = 'curveStepBefore',
+}
+export enum LineStyle {
+    DASHED = 'dashed',
+    SOLID = 'solid',
+}
