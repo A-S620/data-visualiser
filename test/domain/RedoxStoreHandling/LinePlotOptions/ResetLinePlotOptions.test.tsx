e35dfa48757d737b96564393675fbbbc1da16ee0
A-S620 <aatikaseedat00@gmail.com>
2021-01-07 15:01:46 +0000

    LinePlotOptions Redux slice handling Did unit tests: GetLinePlotOptions.test.tsx, ResetLinePlotOptions.test.tsx

---
+++ test/domain/RedoxStoreHandling/LinePlotOptions/ResetLinePlotOptions.test.tsx
@@ -0,0 +1,28 @@
+import 'jsdom-global/register';
+
+import CreateLinePlotOptions from '../../../../src/domain/ReduxStoreHandling/LinePlotOptions/CreateLinePlotOptions';
+import GetLinePlotOptions from '../../../../src/domain/ReduxStoreHandling/LinePlotOptions/GetLinePlotOptions';
+import ResetLinePlotOptions from '../../../../src/domain/ReduxStoreHandling/LinePlotOptions/ResetLinePlotOptions';
+import { CurveType, ILinePlotOptions, LineStyle } from '../../../../src/interfaces/plotting/ILinePlotOptions';
+
+describe('ResetLinePlotOptions domain component', () => {
+    it('Should reset the line plot options', () => {
+        const lineOptions: ILinePlotOptions = {
+            xValue: 'test',
+            yValue: 'test2',
+            height: 500,
+            width: 500,
+            colour: '#cd3b55',
+            opacity: 0,
+            curveType: CurveType.curveMonotoneY,
+            lineStyle: LineStyle.SOLID,
+            lineWidth: 2,
+        };
+        const createLinePlotOptions = new CreateLinePlotOptions(lineOptions);
+        createLinePlotOptions.createLinePlotOptions();
+        const resetLinePlotOptions = new ResetLinePlotOptions();
+        resetLinePlotOptions.resetLinePlotOptions();
+        const getLinePlotOptions = new GetLinePlotOptions();
+        expect(getLinePlotOptions.getLinePlotOptions()).toEqual({});
+    });
+});
