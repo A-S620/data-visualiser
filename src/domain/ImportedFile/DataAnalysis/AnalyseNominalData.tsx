7087271c39173cc48737464457e255d6e8a585f8
A-S620 <aatikaseedat00@gmail.com>
2021-01-28 11:37:47 +0000

    AnalyseNominalData domain component + unit tests

---
+++ src/domain/ImportedFile/DataAnalysis/AnalyseNominalData.tsx
@@ -0,0 +1,80 @@
+import { store } from '../../../ReduxStore/store';
+import { NotificationsHandler } from '../../../UIHandling/NotificationsHandler';
+
+export class AnalyseNominalData {
+    private readonly dataFields = store.getState().importedData.dataFields;
+    private readonly dataObjects = store.getState().importedData.dataAsObjects;
+    private nominalFields: any;
+    private nominalDataObjects: Array<object> = [];
+    constructor(nominalFields: Array<string>) {
+        this.nominalFields = nominalFields;
+    }
+    public validateNominalData(): { nominalFields: Array<string>; nominalDataObjects: Array<object> } {
+        if (this.nominalFields.length > 0) {
+            const nominalValues = this.getAllNominalValues();
+            for (const value of nominalValues) {
+                this.nominalDataObjects.push(this.createNominalObject(value, nominalValues));
+            }
+        }
+        return this.getAnalysedNominalData();
+    }
+    private getAllNominalValues(): Array<string> {
+        const nominalValues: Array<string> = [];
+        for (var objectIndex = 0; objectIndex < this.dataObjects.length; objectIndex += 1) {
+            const object = this.dataObjects[objectIndex];
+            for (var fieldIndex = 0; fieldIndex < this.nominalFields.length; fieldIndex += 1) {
+                const field = this.nominalFields[fieldIndex];
+                const nominalValueToAdd = this.getNominalValue(object, field);
+                if (nominalValueToAdd !== '') {
+                    if (!nominalValues.includes(nominalValueToAdd)) {
+                        nominalValues.push(nominalValueToAdd);
+                    }
+                }
+            }
+        }
+
+        return nominalValues;
+    }
+    private getNominalValue(object: object, field: string): string {
+        for (const [key, value] of Object.entries(object)) {
+            if (key === field) {
+                return value;
+            }
+        }
+        return '';
+    }
+    private getNominalValueCount(nominalValue: string): number {
+        let count = 0;
+        for (var objectIndex = 0; objectIndex < this.dataObjects.length; objectIndex += 1) {
+            const object = this.dataObjects[objectIndex];
+            for (const [key, value] of Object.entries(object)) {
+                if (value === nominalValue) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+    private getNominalValuePercent(nominalValue: string, allValues: Array<string>): number {
+        let total = 0;
+        for (const value of allValues) {
+            total += this.getNominalValueCount(value);
+        }
+        const count = this.getNominalValueCount(nominalValue);
+        return Math.round((count / total) * 100);
+    }
+    private createNominalObject(nominalValue: string, allValues: Array<string>): object {
+        return {
+            name: nominalValue,
+            count: this.getNominalValueCount(nominalValue),
+            percent: this.getNominalValuePercent(nominalValue, allValues),
+        };
+    }
+
+    private getAnalysedNominalData(): { nominalFields: Array<string>; nominalDataObjects: Array<object> } {
+        return {
+            nominalFields: this.nominalFields,
+            nominalDataObjects: this.nominalDataObjects,
+        };
+    }
+}
--- test/domain/ImportedFile/DataAnalysis/AnalyseIntervalData.test.tsx
+++ test/domain/ImportedFile/DataAnalysis/AnalyseIntervalData.test.tsx
@@ -5,55 +5,48 @@ import CreateImportedData from '../../../../src/domain/ReduxStoreHandling/Import
 import { IImportedFileData } from '../../../../src/interfaces/import/IImportedFileData';
 import GetAnalysedData from '../../../../src/domain/ReduxStoreHandling/AnalysedData/GetAnalysedData';
 
-//Test data
-const dataAsObjects = [
-    { col1: '32', col2: '45', col3: 'foo' },
-    { col1: '79', col2: '5', col3: 'foo' },
-    { col1: '76', col2: '23', col3: 'foo' },
-];
-const dataFields = ['col1', 'col2', 'col3'];
-const intervalFields = ['col1', 'col2'];
-const dataAsObjects2 = [
-    { col1: '32', col2: 'cool', col3: 'foo' },
-    { col1: '79', col2: '5', col3: 'foo' },
-    { col1: '76', col2: '23', col3: 'foo' },
-];
-const dataFields2 = ['col1', 'col2', 'col3'];
-const intervalFields2 = ['col1', 'col2'];
 describe('AnalyseIntervalData domain component', () => {
     it('should add the interval objects correctly', () => {
         const testData: IImportedFileData = {
-            dataFields: dataFields2,
-            dataAsObjects: dataAsObjects2,
+            dataFields: ['col1', 'col2', 'col3'],
+            dataAsObjects: [
+                { col1: '32', col2: 'cool', col3: 'foo' },
+                { col1: '79', col2: '5', col3: 'foo' },
+                { col1: '76', col2: '23', col3: 'foo' },
+            ],
             dataAsArrays: [],
         };
         const createImportedData = new CreateImportedData(testData);
         createImportedData.createDataFields();
         createImportedData.createDataAsObjects();
-        const analyseData = new AnalyseIntervalData(intervalFields2);
-        expect(analyseData.validateIntervalData()).toEqual([
+        const analyseIntervalData = new AnalyseIntervalData(['col1', 'col2']);
+        expect(analyseIntervalData.validateIntervalData()).toEqual([
             { col1: 79, col2: 5 },
             { col1: 76, col2: 23 },
         ]);
     });
     it('should add the correct objects, if one of the objects has the wrong length', () => {
         const testData: IImportedFileData = {
-            dataFields: dataFields2,
-            dataAsObjects: dataAsObjects2,
+            dataFields: ['col1', 'col2', 'col3'],
+            dataAsObjects: [
+                { col1: '32', col2: 'cool', col3: 'foo' },
+                { col1: '79', col2: '5', col3: 'foo' },
+                { col1: '76', col2: '23', col3: 'foo' },
+            ],
             dataAsArrays: [],
         };
         const createImportedData = new CreateImportedData(testData);
         createImportedData.createDataFields();
         createImportedData.createDataAsObjects();
-        const analyseData = new AnalyseIntervalData(intervalFields2);
-        expect(analyseData.validateIntervalData()).toStrictEqual([
+        const analyseIntervalData = new AnalyseIntervalData(['col1', 'col2']);
+        expect(analyseIntervalData.validateIntervalData()).toStrictEqual([
             { col1: 79, col2: 5 },
             { col1: 76, col2: 23 },
         ]);
     });
     it('should not accept IP addresses as intervals', () => {
         const testData: IImportedFileData = {
-            dataFields: dataFields2,
+            dataFields: ['col1', 'col2', 'col3'],
             dataAsObjects: [
                 { col1: '32', col2: '21.31.54', col3: 'foo' },
                 { col1: '79', col2: '5', col3: 'foo' },
@@ -64,8 +57,8 @@ describe('AnalyseIntervalData domain component', () => {
         const createImportedData = new CreateImportedData(testData);
         createImportedData.createDataFields();
         createImportedData.createDataAsObjects();
-        const analyseData = new AnalyseIntervalData(intervalFields2);
-        expect(analyseData.validateIntervalData()).toStrictEqual([
+        const analyseIntervalData = new AnalyseIntervalData(['col1', 'col2']);
+        expect(analyseIntervalData.validateIntervalData()).toStrictEqual([
             { col1: 79, col2: 5 },
             { col1: 76, col2: 23 },
         ]);
