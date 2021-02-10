f8755d6e3a9c77a9a6f867975779823781b2e5a8
A-S620 <48350000+a-s620@users.noreply.github.com>
2021-02-10 10:41:04 +0000

    Created IMarkSeriesCreateVis and IMarkSeriesOptions interfaces

---
+++ src/Interfaces/plotting/Mark/IMarkSeriesCreateVis.tsx
@@ -0,0 +1,9 @@
+export interface IMarkSeriesCreateVis {
+    data: Array<Object>;
+    height: number;
+    width: number;
+    colour: string;
+    stroke: string;
+    opacity: number;
+    fill: string;
+}
---
+++ src/Interfaces/plotting/Mark/IMarkSeriesOptions.tsx
@@ -0,0 +1,12 @@
+import { yValue } from '../Bar/IBarSeriesOptions';
+
+export interface IMarkSeriesOptions {
+    xValue: string;
+    yValue: yValue;
+    height: number;
+    width: number;
+    colour: string;
+    stroke: string;
+    opacity: number;
+    fill: string;
+}
