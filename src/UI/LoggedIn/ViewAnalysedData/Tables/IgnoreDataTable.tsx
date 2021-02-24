7d2d68216fe35521cda5f6cc4909f2aaf5a8ae8b
A-S620 <48350000+a-s620@users.noreply.github.com>
2021-02-24 19:19:43 +0000

    Refactoring imports

--- src/UI/LoggedIn/ViewAnalysedData/Tables/IgnoreDataTable.tsx
+++ src/UI/LoggedIn/ViewAnalysedData/Tables/IgnoreDataTable.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TablePagination,
+    TableRow,
+    Paper,
+} from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
