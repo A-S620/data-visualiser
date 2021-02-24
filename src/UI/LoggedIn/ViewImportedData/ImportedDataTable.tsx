295516c9efdcd6be05fdb2420e31329d7210851e
A-S620 <48350000+a-s620@users.noreply.github.com>
2021-02-24 08:27:22 +0000

    Fixing imports on ImportedDataTable

--- src/UI/LoggedIn/ViewImportedData/ImportedDataTable.tsx
+++ src/UI/LoggedIn/ViewImportedData/ImportedDataTable.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TablePagination,
+    TableRow,
+    Paper,
+    Box,
+    TableHead,
+} from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
@@ -15,7 +19,6 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
 import { connect } from 'react-redux';
 import ReactJson from 'react-json-view';
-import { Box, TableHead } from '@material-ui/core';
 
 const useStyles1 = makeStyles((theme: Theme) =>
     createStyles({
