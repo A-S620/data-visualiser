42224258c061304d917dc7d9c765dbab171a60d8
A-S620 <aatikaseedat00@gmail.com>
2021-03-14 07:52:22 +0000

    Created IHeatmapSeriesCreateVis interface

---
+++ src/Interfaces/Visualisations/Heatmap/IHeatmapSeriesCreateVis.tsx
@@ -0,0 +1,10 @@
+export interface IHeatmapSeriesCreateVis {
+    data: Array<Object>;
+    height: number;
+    width: number;
+    colourRange: string[];
+    colour: string;
+    stroke: string;
+    opacity: number;
+    fill: string;
+}
