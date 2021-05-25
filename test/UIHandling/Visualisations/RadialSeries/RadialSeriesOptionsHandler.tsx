2a228b74c7834baa742ce8b01ca27782ad4192e9
A-S620 <aatikaseedat00@gmail.com>
2021-05-25 21:11:45 +0100

    Radial series unit tests for RadialSeriesOptionsHandler

--- test/UIHandling/Visualisations/RadialSeries/RadialSeriesOptionsHandler.tsx
+++ test/UIHandling/Visualisations/RadialSeries/RadialSeriesOptionsHandler.tsx
@@ -1,111 +1,90 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
 import 'jsdom-global/register';
-import { IMarkSeriesOptions } from '../../../../src/Interfaces/Visualisations/Mark/IMarkSeriesOptions';
-import { MarkSeriesOptionsHandler } from '../../../../src/UIHandling/Visualisations/MarkSeries/MarkSeriesOptionsHandler';
-import CurrentMarkVisual from '../../../../src/Domain/ReduxStoreHandling/Plotting/Mark/CurrentMarkVisual';
-import MarkSeriesOptions from '../../../../src/Domain/ReduxStoreHandling/Plotting/Mark/MarkSeriesOptions';
+import { IRadialSeriesOptions } from '../../../../src/Interfaces/Visualisations/Radial/IRadialSeriesOptions';
+import { RadialSeriesOptionsHandler } from '../../../../src/UIHandling/Visualisations/Radial/RadialSeriesOptionsHandler';
+import CurrentRadialVisual from '../../../../src/Domain/ReduxStoreHandling/Plotting/Radial/CurrentRadialVisual';
 
-describe('Mark Series Handler UIHandling Component', () => {
+describe('Radial Series Handler UIHandling Component', () => {
     it('Should not give an error if all options are valid', () => {
-        const testOptions: IMarkSeriesOptions = {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 500,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'test',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         const notifications = seriesHandler.validateOptions();
         expect(notifications.notification()).toBe('');
     });
     it('Should give an error if one of the options are invalid', () => {
-        const testOptions: IMarkSeriesOptions = {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 0,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'tes',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         const notifications = seriesHandler.validateOptions();
         expect(notifications.notification()).toBe(
             'The minimum value for Height is 100, the maximum value for Height is 800. The current height is 0'
         );
     });
     it('Should save valid options in the Redux store', () => {
-        const testOptions: IMarkSeriesOptions = {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 500,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'test',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         seriesHandler.validateOptions();
-        const getSeries = new MarkSeriesOptions().get();
+        const getSeries = new CurrentRadialVisual().get();
         expect(getSeries).toBe(testOptions);
     });
     it('Should create the current visualisation in the Redux store if the options are valid', () => {
-        const testOptions: IMarkSeriesOptions = {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 500,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'test',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         seriesHandler.validateOptions();
-        const getCurrentVisual = new CurrentMarkVisual();
+        const getCurrentVisual = new CurrentRadialVisual();
         expect(getCurrentVisual.get()).toEqual({
             data: [],
             height: 500,
             width: 500,
-            stroke: '',
             opacity: 1,
             colour: '',
-            fill: '',
         });
     });
-    it('Should get the mark options from the Redux store', () => {
-        const testOptions: IMarkSeriesOptions = {
+    it('Should get the options from the Redux store', () => {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 500,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'test',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         seriesHandler.validateOptions();
         expect(seriesHandler.getOptions()).toBe(testOptions);
     });
-    it('Should reset the mark options from the Redux store', () => {
-        const testOptions: IMarkSeriesOptions = {
+    it('Should reset the options from the Redux store', () => {
+        const testOptions: IRadialSeriesOptions = {
             colour: '',
-            fill: '',
             height: 500,
-            opacity: 1,
-            stroke: '',
             width: 500,
             xValue: 'test',
             yValue: 'test',
         };
-        const seriesHandler = new MarkSeriesOptionsHandler(testOptions);
+        const seriesHandler = new RadialSeriesOptionsHandler(testOptions);
         seriesHandler.validateOptions();
         seriesHandler.resetOptions();
         expect(seriesHandler.getOptions()).toEqual({});
