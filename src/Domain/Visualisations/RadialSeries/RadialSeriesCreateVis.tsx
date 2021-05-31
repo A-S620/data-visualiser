15212e0982a5659523541725221a95410140a892
A-S620 <aatikaseedat00@gmail.com>
2021-05-31 16:38:35 +0100

    Refactored RadialSeriesCreateVis to include getDataObjectsForField

--- src/Domain/Visualisations/RadialSeries/RadialSeriesCreateVis.tsx
+++ src/Domain/Visualisations/RadialSeries/RadialSeriesCreateVis.tsx
@@ -2,7 +2,6 @@ import { DataHandler } from '../../../Util/DataHandler';
 import { IRadialSeriesVis } from '../../../Interfaces/Visualisations/Radial/IRadialSeriesVis';
 import RadialSeriesOptions from '../../ReduxStoreHandling/Plotting/Radial/RadialSeriesOptions';
 import GetAnalysedData from '../../ReduxStoreHandling/AnalysedData/GetAnalysedData';
-import ImportedData from '../../ReduxStoreHandling/ImportedData/ImportedData';
 
 export class RadialSeriesCreateVis {
     private dataHandler = new DataHandler();
@@ -12,26 +11,21 @@ export class RadialSeriesCreateVis {
             return this.createDefaultOptions();
         }
 
-        const importedFileData = new ImportedData().get().dataObjects;
         return {
-            data: [
-                { angle: 1, radius: 10 },
-                { angle: 2, label: 'Super Custom label', subLabel: 'With annotation', radius: 20 },
-                { angle: 5, radius: 5, label: 'Alt Label' },
-                { angle: 3, radius: 14 },
-                { angle: 5, radius: 12, subLabel: 'Sub Label only' },
-            ],
+            data: this.getDataObjectsForField(options.column),
             height: options.height,
             width: options.width,
-            colour: options.colour,
         };
+    }
+    private getDataObjectsForField(field: string): Array<object> {
+        const binaryDataObjects = new GetAnalysedData().get().binaryDataObjects;
+        return this.dataHandler.createAngleObjectFromColumnPercent(field, binaryDataObjects);
     }
     public createDefaultOptions(): IRadialSeriesVis {
         return {
             data: [],
             height: 0,
             width: 0,
-            colour: '',
         };
     }
 }
