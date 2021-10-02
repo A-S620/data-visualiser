1c8ddc521474226ee211078a9870dac096cd9a01
A-S620 <aatikaseedat00@gmail.com>
2021-10-02 17:10:38 +0100

    minor fix

--- src/UI/LoggedIn/Navigation/Appbar.js
+++ src/UI/LoggedIn/Navigation/Appbar.js
@@ -42,7 +42,7 @@ const useStyles = makeStyles((theme) => ({
 function a11yProps(index) {
     return {
         id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
+        'aria-controls': `simple-tab-panel-${index}`,
     };
 }
 export default function Appbar() {
